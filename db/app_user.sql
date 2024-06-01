ae33844d9eb582d25954be60f7dfd262945f0535
Graham Cole <graham@sgcole.uk>
2024-06-01 09:20:26 +0100

    added commentary

--- db/app_user.sql
+++ db/app_user.sql
@@ -1,3 +1,5 @@
+-- This is for demonstration purposes only and would normally never be included in version control
+-- unless obfuscated in some fashion
 drop user if exists 'graham_demo'@'localhost';
-CREATE USER 'graham_demo'@'localhost' IDENTIFIED BY 'graham_demo';
-GRANT ALL ON *.* TO 'graham_demo'@'localhost' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS 'graham_demo'@'localhost' IDENTIFIED BY 'graham_demo';
+GRANT ALL ON *.* TO 'janet'@'localhost' WITH GRANT OPTION;
