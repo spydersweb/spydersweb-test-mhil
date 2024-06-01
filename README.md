ea83873a575158a1c39d90595cf91a2b3e41b650
Graham Cole <graham@sgcole.uk>
2024-06-01 08:38:52 +0100

    Initial commit with db setup script

---
+++ README.md
@@ -0,0 +1,10 @@
+# Must Have Ideas Ltd - Demo App
+
+A very basic demonstation app using CakePHP as a backend API to store and interact with a local db instance.
+
+The basic premis is a single page app in React will send commands to the API and update the database
+
+# Running the App
+For demo purposes we are using CakePHP built in webserver to save on setup
+
+Then visit `http://localhost:8765` to see the welcome page.
---
+++ db/database.sql
@@ -0,0 +1,21 @@
+-- Generate the base schema
+create schema if not exists `mhil`;
+
+-- create two tables for interacting with
+DROP TABLE IF EXISTS `mhil`.`albums`;
+CREATE TABLE `mhil`.`albums`(
+  `album_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `album_title` VARCHAR(255) NOT NULL,
+  `album_artist` VARCHAR(255) NOT NULL,
+  `album_year` INT NOT NULL,
+  PRIMARY KEY (`album_id`),
+  UNIQUE INDEX `album_id_UNIQUE` (`album_id` ASC) VISIBLE);
+
+-- create a users table
+DROP TABLE IF EXISTS `mhil`.`users`;
+CREATE TABLE `mhil`.`users` (
+	`user_id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(50) NOT NULL,
+    `created` DATETIME NOT NULL DEFAULT current_timestamp,
+    PRIMARY KEY (`user_id`),
+UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC, `username` ASC) VISIBLE);
