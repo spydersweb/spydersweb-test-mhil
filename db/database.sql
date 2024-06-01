7fbd3e7c4726b7f7965dbbce3e06076cdc33f644
Graham Cole <graham@sgcole.uk>
2024-06-01 11:29:14 +0100

    updated the database script for seeding and table creation

--- db/database.sql
+++ db/database.sql
@@ -11,18 +11,6 @@ CREATE TABLE `mhil`.`albums`(
   PRIMARY KEY (`album_id`),
   UNIQUE INDEX `album_id_UNIQUE` (`album_id` ASC) VISIBLE);
 
--- create a users table
-DROP TABLE IF EXISTS `mhil`.`users`;
-CREATE TABLE `mhil`.`users` (
-	`user_id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(50) NOT NULL,
-    `created` DATETIME NOT NULL DEFAULT current_timestamp,
-    PRIMARY KEY (`user_id`),
-UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC, `username` ASC) VISIBLE);
-
 -- Seed the albums table with some demo data
 INSERT INTO `mhil`.`albums` (`album_title`, `album_artist`, `album_year`) VALUES ('Fear Inoculum', 'Tool', '2019');
 INSERT INTO `mhil`.`albums` (`album_title`, `album_artist`, `album_year`) VALUES ('The Fat of the Land', 'The Prodigy', '1997');
-
--- Seed the users table with a single user
-INSERT INTO `mhil`.`users` (`username`) VALUES ('graham');
